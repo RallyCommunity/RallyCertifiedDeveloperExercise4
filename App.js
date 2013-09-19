5220c379ed4faf1c0c9c6ea1cc5d684cd1fdc1d5
David P Thomas <davidpthomas@gmail.com>
2013-09-19 05:11:23 -0400

    solution to chart

--- App.js
+++ App.js
@@ -6,24 +6,65 @@ Ext.define('CustomApp', {
         this.getData();
     },
     getData: function() {
-        // TODO: wsapi data store; on load, aggregate data
-        this.aggregateData();
+        
+        var myFilters = Ext.create('Rally.data.QueryFilter', {
+             property: 'ScheduleState',
+             operator: '=',
+             value: 'Defined'
+        });
+        var myFilters = myFilters.or(
+            Ext.create('Rally.data.QueryFilter', {
+                 property: 'ScheduleState',
+                 operator: '=',
+                 value: 'In-Progress'
+            }) 
+        );
+        var myFilters = myFilters.or(
+            Ext.create('Rally.data.QueryFilter', {
+                 property: 'ScheduleState',
+                 operator: '=',
+                 value: 'Completed'
+            }) 
+        );
+        var myFilters = myFilters.or(
+            Ext.create('Rally.data.QueryFilter', {
+                 property: 'ScheduleState',
+                 operator: '=',
+                 value: 'Accepted'
+            }) 
+        );
+
+        Ext.create('Rally.data.WsapiDataStore', {
+            model: 'User Story',
+            autoLoad: true,
+            listeners: {
+                load: function(store, records, success) {
+                    this.aggregateData(records);
+                },
+                scope: this
+            },
+            filters: myFilters,
+            fetch: ['Name', 'ScheduleState']
+        });
+
     },
     aggregateData: function(storyRecords) {
-        // TODO: bucket stories by schedule state; render chart
         
+        var myData = {
+            'Defined': 0,
+            'In-Progress': 0,
+            'Completed': 0,
+            'Accepted': 0
+        };
         
-        // example data structure format to send to report render function
-        var mockData = {        
-            'backlog': 10,
-            'inprogress': 5,
-            'completed': 15,
-            'accepted': 20
-        }
-        
-        this.renderChart(mockData);
+        Ext.Array.each(storyRecords, function(record) {
+            myData[record.get("ScheduleState")]++;
+        });
+
+        this.renderChart(myData);
     },
     renderChart: function(myData) {
+        console.log('foo', myData);
         var myChartConfig = {
             chart: {
                 type: 'column'
@@ -64,16 +105,16 @@ Ext.define('CustomApp', {
         var myChartData = {
             series: [{
                 name: 'Backlog',
-                data: [myData.backlog]
+                data: [myData['Defined']]
             },{
                 name: 'In-Progress',
-                data: [myData.inprogress]
+                data: [myData['In-Progress']]
             }, {
                 name: 'Completed',
-                data: [myData.completed]
+                data: [myData['Completed']]
             }, {
                 name: 'Accepted',
-                data: [myData.accepted]
+                data: [myData['Accepted']]
             },]
         };
 
